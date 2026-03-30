d6b75fa7cc1ec9aa841b93af06a3523e11df1ef0
dienoov <alvinodien@yahoo.co.id>
2026-03-30 11:48:53 +0700

    feat: enhance dashboard visualizations

--- dashboard.py
+++ dashboard.py
@@ -14,40 +14,6 @@ col1.metric('Total Data', len(df))
 col2.metric('Dropout %', round((df['Status']=='Dropout').mean()*100,2))
 col3.metric('Graduate %', round((df['Status']=='Graduate').mean()*100,2))
 
-col1, col2, col3 = st.columns(3)
-
-col1.subheader('Distribution of Status')
-fig = px.pie(
-    df,
-    names='Status',
-)
-col1.plotly_chart(fig, use_container_width=True)
-
-col2.subheader('Distribution of Age')
-fig = px.histogram(
-    df,
-    x='Age_at_enrollment',
-    nbins=20,
-)
-col2.plotly_chart(fig, use_container_width=True)
-
-df['Gender'] = df['Gender'].map({0: 'Female', 1: 'Male'})
-col3.subheader('Distribution of Gender')
-fig = px.pie(
-    df,
-    names='Gender',
-)
-col3.plotly_chart(fig, use_container_width=True)
-
-st.subheader('Distribution of Admission Grade')
-fig = px.histogram(
-    df,
-    x='Admission_grade',
-    color='Status',
-    barmode='overlay',
-)
-st.plotly_chart(fig, use_container_width=True)
-
 col1, col2 = st.columns(2)
 
 col1.subheader('Approved Units (1st Semester)')
@@ -68,7 +34,46 @@ fig = px.box(
 )
 col2.plotly_chart(fig, use_container_width=True)
 
-col1, col2 = st.columns(2)
+col1.subheader('Grades (1st Semester)')
+fig = px.box(
+    df,
+    x='Status',
+    y='Curricular_units_1st_sem_grade',
+    color='Status',
+)
+col1.plotly_chart(fig, use_container_width=True)
+
+col2.subheader('Grades (2nd Semester)')
+fig = px.box(
+    df,
+    x='Status',
+    y='Curricular_units_2nd_sem_grade',
+    color='Status',
+)
+col2.plotly_chart(fig, use_container_width=True)
+
+df['Gender'] = df['Gender'].map({0: 'Female', 1: 'Male'})
+col1.subheader('Distribution of Gender')
+fig = px.histogram(
+    df,
+    x='Gender',
+    color='Status',
+    barmode='group',
+)
+col1.plotly_chart(fig, use_container_width=True)
+
+col2.subheader('Distribution of Age at Enrollment')
+fig = px.histogram(
+    df,
+    x='Age_at_enrollment',
+    nbins=20,
+    color='Status',
+    barmode='overlay',
+    opacity=0.7,
+)
+col2.plotly_chart(fig, use_container_width=True)
+
+col1, col2, col3 = st.columns(3)
 
 col1.subheader('Debtor Status')
 df['Debtor'] = df['Debtor'].map({0: 'No', 1: 'Yes'})
@@ -89,3 +94,12 @@ fig = px.histogram(
     barmode='group',
 )
 col2.plotly_chart(fig, use_container_width=True)
+
+col3.subheader('Tuition Fees Up to Date')
+fig = px.histogram(
+    df,
+    x='Tuition_fees_up_to_date',
+    color='Status',
+    barmode='group',
+)
+col3.plotly_chart(fig, use_container_width=True)
